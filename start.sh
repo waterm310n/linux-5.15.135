d9de7487e125ac030ec512b36aac38f60cb3295c
Shi Lei <shi_lei@massclouds.com>
2024-07-23 06:38:07 +0000

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 QEMU=/home/cloud/gitWork/qemu/build/qemu-system-riscv64
 #QEMU=qemu-system-riscv64
 
+if [ $# -ne 1 ]; then
+  echo "$0 [app_name]";
+  exit;
+fi
+
 ${QEMU} -m 128M -smp 1 -machine virt \
 -bios default -kernel ./output/vmlinuz-5.15.135+ \
 -device virtio-blk-device,drive=disk0 -drive id=disk0,if=none,format=raw,file=./payload/disk.img \
--nographic -append "earlycon=sbi root=/dev/vda rw console=ttyS0 init=/sbin/vfork" -D qemu.log -d int
+-nographic -append "earlycon=sbi root=/dev/vda rw console=ttyS0 init=$1"
 #-nographic -append "earlycon=sbi root=/dev/vda rw console=ttyS0" -D qemu.log -d int
 #-nographic -append "earlycon=sbi root=/dev/vda rw console=ttyS0 init=/sbin/mmap" -D qemu.log -d in_asm,int
 #-nographic -append "earlycon=sbi root=/dev/vda rw console=ttyS0 init=/sbin/sh" -D qemu.log -d in_asm,int
